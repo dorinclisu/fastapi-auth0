aca3877741c54a45bc31611633d6f20875dd3feb
dorin.clisu <dorin.clisu@gmail.com>
2021-12-19 15:58:30 +0200

    Add package typing marker for mypy, bump version

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ with open('README.md', encoding='utf-8') as file:
 
 setuptools.setup(
     name='fastapi-auth0',
-    version='0.2.1',
+    version='0.3.0',
     description='Easy auth0.com integration for FastAPI',
     long_description=readme,
     long_description_content_type='text/markdown',
     url='https://github.com/dorinclisu/fastapi-auth0',
     author='Dorin Clisu',
-    author_email='dorin.clisu@gmail.com',
+    author_email='gmail dorin . clisu',
     license='GNU-GPL-V3',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
+    package_data={'': ['py.typed']},
     python_requires='>=3.6',
     install_requires=['fastapi>=0.60.0', 'python-jose>=3.2.0']
 )
