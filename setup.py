0a361065c922328be988262f21068afc88487f6b
dorin.clisu <dorin.clisu@gmail.com>
2022-11-27 21:21:06 +0200

    Change to MIT license also in pypy metadata

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open('README.md', encoding='utf-8') as file:
 
 setuptools.setup(
     name='fastapi-auth0',
-    version='0.3.1',
+    version='0.3.2',
     description='Easy auth0.com integration for FastAPI',
     long_description=readme,
     long_description_content_type='text/markdown',
     url='https://github.com/dorinclisu/fastapi-auth0',
     author='Dorin Clisu',
-    license='GNU-GPL-V3',
+    license='MIT',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     package_data={'': ['py.typed']},
