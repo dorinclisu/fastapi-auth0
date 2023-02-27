8e2f4faae7a26477a8d10a8d93e04f1e9d898876
Sandro Braun <supermario94123@gmail.com>
2023-02-27 18:01:38 +0100

    Update malformed token

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -297,4 +297,4 @@ def test_token():
     resp = client.get('/secure', headers=get_bearer_header(invalid_token))
     assert resp.status_code == 401, resp.text
     error_detail = resp.json()['detail']
-    assert 'kid header not provided.' in error_detail.lower(), error_detail
+    assert 'malformed' in error_detail.lower(), error_detail
