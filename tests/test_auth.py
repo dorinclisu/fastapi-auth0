fa96adfcda0749ecf2aec093ebf0f1aa8c496a68
dorin.clisu <dorin.clisu@gmail.com>
2023-03-07 12:16:47 +0200

    Improve test token decoding and encoding

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -88,11 +88,11 @@ def get_bearer_header(token: str) -> Dict[str, str]:
 
 def get_malformed_token(token: str) -> str:
     payload_encoded = token.split('.')[1]
-    payload_str = base64.b64decode(payload_encoded + '=' * (4 - len(payload_encoded) % 4)).decode()
+    payload_str = base64.b64decode(f'{payload_encoded}==').decode()
     payload = json.loads(payload_str)
 
     payload['sub'] = 'evil'
-    bad_payload_str = json.dumps(payload)
+    bad_payload_str = json.dumps(payload, separators=(',', ':'))
     bad_payload_encoded = base64.b64encode(bad_payload_str.encode()).decode().replace('=', '')
 
     return token.replace(payload_encoded, bad_payload_encoded)
@@ -100,11 +100,11 @@ def get_malformed_token(token: str) -> str:
 
 def get_missing_kid_token(token: str) -> str:
     header_encoded = token.split('.')[0]
-    header_str = base64.b64decode(header_encoded + '=' * (4 - len(header_encoded) % 4)).decode()
+    header_str = base64.b64decode(f'{header_encoded}==').decode()
     header = json.loads(header_str)
 
-    header.pop("kid")
-    bad_header_str = json.dumps(header)
+    header.pop('kid')
+    bad_header_str = json.dumps(header, separators=(',', ':'))
     bad_header_encoded = base64.b64encode(bad_header_str.encode()).decode().replace('=', '')
 
     return token.replace(header_encoded, bad_header_encoded)
