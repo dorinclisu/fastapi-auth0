cfb31fb29a420d5c2e2ed8c4d65d1f3c19af776d
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2023-11-23 09:55:20 +0100

    fix: explicitly export public models/variables

--- src/fastapi_auth0/__init__.py
+++ src/fastapi_auth0/__init__.py
@@ -1,2 +1,11 @@
 from .auth import Auth0, Auth0User, Auth0UnauthenticatedException, Auth0UnauthorizedException
 from .auth import security_responses, auth0_rule_namespace
+
+__all__ = [
+    "Auth0",
+    "Auth0User",
+    "Auth0UnauthenticatedException",
+    "Auth0UnauthorizedException",
+    "security_responses",
+    "auth0_rule_namespace",
+]
